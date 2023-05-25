09fe3e588fe5515449a99cd459283753ec3cc777
Prateek Takthar <prateektakthar184b@gmail.com>
2023-05-26 00:03:57 +0530

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,39 +1,67 @@
-// import { useState } from "react";
+import { useState, onFormUpdate } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg"
 
 export const Contact = () => {
-    // const formInitialDetails = {
-    //     firstName: '',
-    //     lastName: '',
-    //     email: '',
-    //     phone: '',
-    //     message: ''
-    // }
+    const formInitialDetails = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        message: ''
+    }
 
 
-// const [formDetails, setFormDetails] = useState(formInitialDetails);
-// const [buttonText,setStatus] = useState({});
+    const [formDetails, setFormDetails] = useState(formInitialDetails);
+    const [ buttonText, setStatus] = useState({});
 
-return(
-    <section className="contact" id="connect">
-        <Container>
-            <Row className="align-items-center">
-                <Col md={6}>
-                    <img src={contactImg} alt="contact us" />
-                </Col>
-                <Col md={6}>
-                    <h2>Get in Touch</h2>
-                    <form >
-                        <Row>
-                            
-                        </Row>
-                    </form>
-                </Col>
-            </Row>
-        </Container>
-    </section>
-)
+    const onFormUpdate = (category, value) => {
+        setFormDetails({
+            ...formDetails,
+            [category]: value
+        })
+    }
+
+    return (
+        <section className="contact" id="connect">
+            <Container>
+                <Row className="align-items-center">
+                    <Col md={6}>
+                        <img src={contactImg} alt="contact us" />
+                    </Col>
+                    <Col md={6}>
+                        <h2>Get in Touch</h2>
+                        <form >
+                            <Row>
+                                <Col sm={6} className="px-1">
+                                    <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                                </Col>
+                                <Col sm={6} className="px-1">
+                                    <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                                </Col>
+                                <Col sm={6} className="px-1">
+                                    <input type="email" value={formDetails.email} placeholder="Enter your email" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                                </Col>
+                                <Col sm={6} className="px-1">
+                                    <input type="tell" value={formDetails.phone} placeholder="Phone" onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                                </Col>
+                                <Col>
+                                    <textarea row="6" value={formDetails.message} placeholder="Enter your Message" onChange={(e) => onFormUpdate('message', e.target.value)} />
+                                    <button type="submit"><span>{buttonText}</span></button>
+                                </Col>
+                                {
+                                    status.message &&
+                                    <Col>
+                                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                                    </Col>
+                                }
+                            </Row>
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
 
 
 }
