d070c2fc3f431ec3d0243c49d6bdd55b78733bb3
Prateek Takthar <prateektakthar184b@gmail.com>
2023-05-27 16:31:04 +0530

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,30 @@
-import { useState, onFormUpdate } from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import contactImg from "../assets/img/contact-img.svg"
+import contactImg from "../assets/img/contact-img.svg";
 
 export const Contact = () => {
     const formInitialDetails = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        message: ''
-    }
-
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        message: ""
+    };
 
     const [formDetails, setFormDetails] = useState(formInitialDetails);
-    const [ buttonText, setStatus] = useState({});
+    const [status, setStatus] = useState({});
 
-    const onFormUpdate = (category, value) => {
+    const handleFormUpdate = (category, value) => {
         setFormDetails({
             ...formDetails,
             [category]: value
-        })
-    }
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission logic
+    };
 
     return (
         <section className="contact" id="connect">
@@ -31,37 +35,94 @@ export const Contact = () => {
                     </Col>
                     <Col md={6}>
                         <h2>Get in Touch</h2>
-                        <form >
+                        <form onSubmit={handleSubmit}>
                             <Row>
                                 <Col sm={6} className="px-1">
-                                    <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                                    <input
+                                        type="text"
+                                        value={formDetails.firstName}
+                                        placeholder="First Name"
+                                        onChange={(e) =>
+                                            handleFormUpdate(
+                                                "firstName",
+                                                e.target.value
+                                            )
+                                        }
+                                    />
                                 </Col>
                                 <Col sm={6} className="px-1">
-                                    <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                                    <input
+                                        type="text"
+                                        value={formDetails.lastName}
+                                        placeholder="Last Name"
+                                        onChange={(e) =>
+                                            handleFormUpdate(
+                                                "lastName",
+                                                e.target.value
+                                            )
+                                        }
+                                    />
                                 </Col>
                                 <Col sm={6} className="px-1">
-                                    <input type="email" value={formDetails.email} placeholder="Enter your email" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                                    <input
+                                        type="email"
+                                        value={formDetails.email}
+                                        placeholder="Enter your email"
+                                        onChange={(e) =>
+                                            handleFormUpdate(
+                                                "email",
+                                                e.target.value
+                                            )
+                                        }
+                                    />
                                 </Col>
                                 <Col sm={6} className="px-1">
-                                    <input type="tell" value={formDetails.phone} placeholder="Phone" onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                                    <input
+                                        type="tel"
+                                        value={formDetails.phone}
+                                        placeholder="Phone"
+                                        onChange={(e) =>
+                                            handleFormUpdate(
+                                                "phone",
+                                                e.target.value
+                                            )
+                                        }
+                                    />
                                 </Col>
                                 <Col>
-                                    <textarea row="6" value={formDetails.message} placeholder="Enter your Message" onChange={(e) => onFormUpdate('message', e.target.value)} />
-                                    <button type="submit"><span>{buttonText}</span></button>
+                                    <textarea
+                                        rows="6"
+                                        value={formDetails.message}
+                                        placeholder="Enter your Message"
+                                        onChange={(e) =>
+                                            handleFormUpdate(
+                                                "message",
+                                                e.target.value
+                                            )
+                                        }
+                                    />
+                                    <button type="submit">
+                                        <span>{buttonText}</span>
+                                    </button>
                                 </Col>
-                                {
-                                    status.message &&
+                                {status.message && (
                                     <Col>
-                                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                                        <p
+                                            className={
+                                                status.success === false
+                                                    ? "danger"
+                                                    : "success"
+                                            }
+                                        >
+                                            {status.message}
+                                        </p>
                                     </Col>
-                                }
+                                )}
                             </Row>
                         </form>
                     </Col>
                 </Row>
             </Container>
         </section>
-    )
-
-
-}
+    );
+};
